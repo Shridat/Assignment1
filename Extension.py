6f52f6477260ff19030fce0d7d56a2f464e00aef
Shridat <80768804+Shridat@users.noreply.github.com>
2021-06-05 15:55:53 +0530

    Create Extension.py

---
+++ Extension.py
@@ -0,0 +1,11 @@
+file_name=str(input("Input the Filename:"))
+if(file_name.split('.')[1]=='c'):
+  print('The extension of the file is C')
+if(file_name.split('.')[1]=='cpp'):
+  print('The extension of the file is C++')
+if(file_name.split('.')[1]=='java'):
+  print('The extension of the file is Java')
+if(file_name.split('.')[1]=='py'):
+  print('The extension of the file is python')
+if(file_name.split('.')[1]=='html'):
+  print('The extension of the file is HTMl')
